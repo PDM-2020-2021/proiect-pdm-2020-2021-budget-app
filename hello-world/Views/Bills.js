51968e5cdfd64179cae73f5b6ac53d3258c5754b
Ilinca <ilinca-florinela.turcu@student.tuiasi.ro>
2020-11-27 16:20:38 +0200

    in progress...nu merge

--- hello-world/Views/Bills.js
+++ hello-world/Views/Bills.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
-import {View, TouchableOpacity, Text, StyleSheet, TouchableHighlight} from 'react-native';
-import {Agenda} from 'react-native-calendars';
-import {Card, Avatar} from 'react-native-paper';
+import React, { useState } from 'react';
+import { View, TouchableOpacity, Text, StyleSheet, TouchableHighlight } from 'react-native';
+import { Agenda } from 'react-native-calendars';
+import { Card, Avatar } from 'react-native-paper';
 
 const styles = StyleSheet.create({
   header: {
@@ -10,61 +10,61 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start',
     justifyContent: 'center',
     flexDirection: 'row'
-},
-headerText: {
-  flex: 1,
-  alignSelf:'flex-start',
-  marginTop:60,
-  marginLeft:10,
-  fontSize: 40,
+  },
+  headerText: {
+    flex: 1,
+    alignSelf: 'flex-start',
+    marginTop: 60,
+    marginLeft: 10,
+    fontSize: 40,
     fontWeight: 'bold',
     color: 'white',
-},
-item: {
-  flexDirection: 'row',
-  borderBottomWidth: 1,
-  borderBottomColor: 'white',
-  alignItems: 'center',
-  
-},
-addButton:{
-  borderWidth:1,
-  borderColor:'rgba(0,0,0,0.2)',
-  alignItems:'center',
-  justifyContent:'center',
-  alignSelf: 'flex-end',
-  width:70,
-  height:70,
-  backgroundColor:'#4fa2d2',
-  borderRadius:50,
-  marginBottom:10
-},
-text: {
-  marginVertical: 30,
-  fontSize: 20,
-  fontWeight: 'bold',
-  marginLeft: 10,
-},
+  },
+  item: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: 'white',
+    alignItems: 'center',
+
+  },
+  addButton: {
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,0.2)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'flex-end',
+    width: 70,
+    height: 70,
+    backgroundColor: '#4fa2d2',
+    borderRadius: 50,
+    marginBottom: 10
+  },
+  text: {
+    marginVertical: 30,
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
 });
 const timeToString = (time) => {
   const date = new Date(time);
   return date.toISOString().split('T')[0];
 };
 
-const Bills = props  => {
+const Bills = props => {
   const [items, setItems] = useState({});
 
   const loadItems = (day) => {
     setTimeout(() => {
-    
-        const time = day.timestamp ;
-        const strTime = timeToString(time);
-        if (!items[strTime]) {
-          items[strTime] = [];
-            items[strTime].push({
-              name: 'Factura la ' + strTime + ' #',
-              
-            });
+
+      const time = day.timestamp;
+      const strTime = timeToString(time);
+      if (!items[strTime]) {
+        items[strTime] = [];
+        items[strTime].push({
+          name: 'Factura la ' + strTime + ' #',
+
+        });
       }
       const newItems = {};
       Object.keys(items).forEach((key) => {
@@ -76,7 +76,7 @@ const Bills = props  => {
 
   const renderItem = (item) => {
     return (
-      <TouchableOpacity style={{marginRight: 10, marginTop: 17}}>
+      <TouchableOpacity style={{ marginRight: 10, marginTop: 17 }}>
         <Card>
           <Card.Content>
             <View
@@ -95,26 +95,26 @@ const Bills = props  => {
   };
 
   return (
-    <View style={{flex: 1}}>
-       <View style={styles.header}>
-          <Text style={styles.headerText}>Bills</Text>
-          <TouchableHighlight  
-                style={styles.addButton}
-                onPress={() => {this.setModalVisible(true); this.setInputText(),this.setIsNew(true)}} underlayColor={'#f1f1f1'}> 
-                    <View style={styles.item} >
-                        <Text style={styles.text}>+  </Text>
-                    </View>
-                    </TouchableHighlight>
-        </View>
-    <View style={{flex: 1}}>
-      <Agenda
-        items={items}
-        loadItemsForMonth={loadItems}
-        selected={'2020-11-25'}
-        renderItem={renderItem}
-      />
-    </View>
-    
+    <View style={{ flex: 1 }}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Bills</Text>
+        <TouchableHighlight
+          style={styles.addButton}
+          onPress={() => { this.setModalVisible(true); this.setInputText(), this.setIsNew(true) }} underlayColor={'#f1f1f1'}>
+          <View style={styles.item} >
+            <Text style={styles.text}>+  </Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+      <View style={{ flex: 1 }}>
+        <Agenda
+          items={items}
+          loadItemsForMonth={loadItems}
+          selected={'2020-11-25'}
+          renderItem={renderItem}
+        />
+      </View>
+
     </View>
   );
 };
